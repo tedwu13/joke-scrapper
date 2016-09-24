5e42baf02082a60d8f4d5e66c5dcd44a7b828434
Ted Wu <tedwu2016@u.northwestern.edu>
2016-09-23 23:43:18 -0700

    Added Joke scraping for some urls

--- scrape.py
+++ scrape.py
@@ -1,11 +1,10 @@
 from bs4 import BeautifulSoup
 import urllib
 r = urllib.urlopen('http://www.laughfactory.com/jokes/popular-jokes').read()
-soup = BeautifulSoup(r)
-jokes = soup.find_all("div", {'class': 'joke-test'})
+soup = BeautifulSoup(r, "lxml")
+# jokes = soup.find_all("div", {'class': 'joke-text'})
 
-
-# Get all the jokes-nav urls
+# Get all the urls from the side navigation bar
 
 def get_jokes_nav_urls():
     urls = [] 
@@ -13,11 +12,18 @@ def get_jokes_nav_urls():
         for l in link.find_all('a', href=True):
             urls.append(l.get('href'))
     return urls
-def set_scraping_url():
-    urls = get_jokes_nav_urls()
-    
-    # for joke_url in urls:
-        # data = urllib.urlopen(joke_url).read()
-    # print links
 
+def set_scraping_url():
+    print "Starting scraping process....."
+    # urls = get_jokes_nav_urls()
+    urls = ['http://www.laughfactory.com/jokes/popular-jokes', 
+    'http://www.laughfactory.com/jokes/latest-jokes']
+
+    jokes = []
+    for joke_url in urls:
+        r = urllib.urlopen(joke_url).read()
+        soup = BeautifulSoup(r, "lxml")
+        print soup.find_all("div", {'class': 'joke-text'})
+        # jokes.append(soup.find_all("div", {'class': 'joke-text'}))
+        # print jokes
 set_scraping_url();
